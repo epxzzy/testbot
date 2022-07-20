8fc6d738774f9179ef45a5ac21462487b9cba359
epizyy <82944668+Epizy66@users.noreply.github.com>
2022-07-20 11:23:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -123,3 +123,17 @@ client.on("messageCreate", async (message) => {
     });
 });
 client.login(process.env.TOKEN); //login using the token
+const express = require("express")
+
+const server = express()
+
+server.all("/", (req, res) => {
+  res.send("Bot is running!")
+})
+
+function runnbitch() {
+  server.listen(3000, () => {
+    console.log("Server is ready.")
+  })
+}
+runnbitch();
